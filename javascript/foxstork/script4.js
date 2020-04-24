c8e66db590563072606c141c078fa52030f34530
Rachel Nitzsche <rachel.nitzsche@wustl.edu>
2020-04-24 04:42:05 -0500

    stork4

---
+++ javascript/foxstork/script4.js
@@ -0,0 +1,13 @@
+var animation = anime({
+  targets: '.foxwalkaway',
+  translateX: 600,
+  elasticity: 200,
+  easing: 'linear',
+  autoplay: false
+});
+
+var seekProgress = document.querySelector('.progress');
+
+seekProgress.oninput = function() {
+  animation.seek(animation.duration * (seekProgress.value / 100));
+};
