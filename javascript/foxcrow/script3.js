b0e0fac82549b66d89a9846be7a65563bf427091
Rachel Nitzsche <rachel.nitzsche@wustl.edu>
2020-04-24 02:32:26 -0500

    crow3

---
+++ javascript/foxcrow/script3.js
@@ -0,0 +1,13 @@
+var animation = anime({
+  targets: '.cheesedrop',
+  translateY: 200,
+  elasticity: 200,
+  easing: 'linear',
+  autoplay: false
+});
+
+var seekProgress = document.querySelector('.progress');
+
+seekProgress.oninput = function() {
+  animation.seek(animation.duration * (seekProgress.value / 100));
+};
